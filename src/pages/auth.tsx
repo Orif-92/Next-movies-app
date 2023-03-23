2dcd5ac3595a0ea5d9f765604d92e786cada858a
Orif-92 <sarniyozovorif@gmail.com>
2023-03-23 22:57:54 +0300

    Hero component

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,18 +1,11 @@
 import Head from 'next/head';
-import AuthComponent from 'src/components/auth';
 
 const Auth = () => {
 	return (
-		<>
-			<Head>
-				<title>Auth</title>
-				<meta name='description' content='For watching movies you should sign to app' />
+	@@ -10,7 +9,6 @@ const Auth = () => {
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
-			<AuthComponent />
 		</>
 	);
 };
-
-export default Auth;
