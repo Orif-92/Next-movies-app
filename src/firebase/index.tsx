07dbbf5ad2bc913b5d8b55b2121c25c8f0c34a18
Orif-92 <sarniyozovorif@gmail.com>
2023-03-25 21:55:30 +0300

    Formik validation

---
+++ src/firebase/index.tsx
@@ -0,0 +1,19 @@
+import { initializeApp, getApp, getApps } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCC4C20yJPa9f5--ukGJmv5CxtOgIqEHHo",
+  authDomain: "sammi-movie-app-cbb53.firebaseapp.com",
+  projectId: "sammi-movie-app-cbb53",
+  storageBucket: "sammi-movie-app-cbb53.appspot.com",
+  messagingSenderId: "1074128102099",
+  appId: "1:1074128102099:web:22c39c4c2baf97f7d4aa26"
+};
+
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore();
+const auth = getAuth();
+
+export default app;
+export { db, auth };
